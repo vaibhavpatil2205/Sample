a73273cb45923d02a42912533bcb93d1a059b24a
unknown <vaibhavpatil5471@gmail.com>
2021-06-26 07:05:56 +0100

    Added

---
+++ Association.java
@@ -0,0 +1,46 @@
+class Bank
+{
+    private String name;
+
+    // bank name
+    Bank(String name)
+    {
+        this.name = name;
+    }
+
+    public String getBankName()
+    {
+        return this.name;
+    }
+}
+
+// employee class
+class Employee
+{
+    private String name;
+
+    // employee name
+    Employee(String name)
+    {
+        this.name = name;
+    }
+
+    public String getEmployeeName()
+    {
+        return this.name;
+    }
+}
+
+// Association between both the
+// classes in main method
+class Association
+{
+    public static void main (String[] args) 
+    {
+        Bank bank = new Bank("Axis");
+        Employee emp = new Employee("Sunny");
+
+        System.out.println(emp.getEmployeeName() + 
+               " is employee of " + bank.getBankName());
+    }
+}
